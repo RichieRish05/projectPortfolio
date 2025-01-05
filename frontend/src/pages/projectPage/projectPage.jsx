37bd92f488ad8a64613afcdc86692aead97ab3a1
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-01-04 20:59:24 -0800

    Netlify fixes

--- frontend/src/pages/projectPage/projectPage.jsx
+++ frontend/src/pages/projectPage/projectPage.jsx
@@ -41,7 +41,7 @@ const ProjectPage = () => {
 
     const mediaContent = {
         "chess": 
-                <video width="320" height="240" autoPlay playsinline controls loop>
+                <video width="320" height="240" autoPlay playsInline controls loop>
                     <source src="/chessPlay.mp4" type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
